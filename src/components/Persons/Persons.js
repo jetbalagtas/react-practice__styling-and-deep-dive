8be3f42287f2cc24e5cb7187acebc28821dd4f51
jetbalagtas@gmail.com <jetstark@Jets-MacBook-Pro.local>
2018-07-15 16:37:18 -0400

    performance gains with PureComponents

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 
 import Person from './Person/Person';
 
-class Persons extends Component {
+class Persons extends PureComponent {
   constructor(props) {
     super(props);
     console.log('[Persons.js] Inside Constructor');
@@ -20,13 +20,15 @@ class Persons extends Component {
     console.log('[UPDATE Persons.js] Inside componentWillReceiveProps', nextProps);
   }
 
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log('[UPDATE Persons.js] Inside shouldComponentUpdate', nextProps, nextState);
-    return nextProps.persons !== this.props.persons ||
-      nextProps.changed !== this.props.changed ||
-      nextProps.click !== this.props.click;
-    // return true; // returning true here causes the lifecycle to run even if there are no changes, costing performance
-  }
+  // the built-in PureComponent (imported with React above then extended as a class) will automatically check for prop and state changes
+  // you should only use PureComponent if you know that updates might not be required, and also must be strategically placed within the app tree
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   console.log('[UPDATE Persons.js] Inside shouldComponentUpdate', nextProps, nextState);
+  //   return nextProps.persons !== this.props.persons ||
+  //     nextProps.changed !== this.props.changed ||
+  //     nextProps.click !== this.props.click;
+  //   // return true; // returning true here causes the lifecycle to run even if there are no changes, costing performance
+  // }
 
   componentWillUpdate(nextProps, nextState) {
     console.log('[UPDATE Persons.js] Inside componentWillUpdate', nextProps, nextState);
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 
-class App extends Component {
+class App extends PureComponent {
   constructor(props) {
     super(props);
     console.log('[App.js] Inside Constructor');
@@ -26,12 +26,14 @@ class App extends Component {
     console.log('[App.js] Inside componentDidMount()');
   }
 
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log('[UPDATE App.js] Inside shouldComponentUpdate', nextProps, nextState);
-    return nextState.persons !== this.state.persons ||
-      nextState.showPersons !== this.state.showPersons;
-    // return true; // returning true here causes the lifecycle to run even if there are no changes, costing performance
-  }
+  // the built-in PureComponent (imported with React above then extended as a class) will automatically check for prop and state changes
+  // you should only use PureComponent if you know that updates might not be required, and also must be strategically placed within the app tree
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   console.log('[UPDATE App.js] Inside shouldComponentUpdate', nextProps, nextState);
+  //   return nextState.persons !== this.state.persons ||
+  //     nextState.showPersons !== this.state.showPersons;
+  //   // return true; // returning true here causes the lifecycle to run even if there are no changes, costing performance
+  // }
 
   componentWillUpdate(nextProps, nextState) {
     console.log('[UPDATE App.js] Inside componentWillUpdate', nextProps, nextState);
